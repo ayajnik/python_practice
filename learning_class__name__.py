41cc46f185105de66fe744c7aa27547dc9f40c54
Ayush Yajnik <46913656+ayajnik@users.noreply.github.com>
2019-11-15 16:34:37 -0500

    Add files via upload

---
+++ learning_class__name__.py
@@ -0,0 +1,16 @@
+class Learning():
+
+
+
+     def method1(self):
+          a = int(input('Enter the first number : '))
+          b = int(input('Enter the second number : '))
+
+          print('The sum of the first two number is as follows : ')
+          print('\n')
+          return int(a + b)
+
+     
+
+if __name__ == '__main__':
+     Learning()
